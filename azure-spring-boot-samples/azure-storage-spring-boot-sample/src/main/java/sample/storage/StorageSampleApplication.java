dbcc05b606832d4fc8363db1c92f804f25923f4f
Wojciech Grajewski <wojciech.grajewski@dynatrace.com>
2020-05-06 14:20:52 +0200

    Enable choosing custom application.properties when running a jar

--- azure-spring-boot-samples/azure-storage-spring-boot-sample/src/main/java/sample/storage/StorageSampleApplication.java
+++ azure-spring-boot-samples/azure-storage-spring-boot-sample/src/main/java/sample/storage/StorageSampleApplication.java
@@ -10,10 +10,17 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Currently only /download endpoint works if running the app from .jar
+ * In this case first manually upload a sample storageTestFile.txt to your chosen container (e.g. through Azure Portal)
+ *
+ * To use different application.properties when running a JAR:
+ * java -jar .\target\azure-storage-spring-boot-sample-0.0.1-SNAPSHOT.jar --spring.config.location=<path/to/application.properties>
+ */
 @SuppressFBWarnings({"RV_RETURN_VALUE_IGNORED"})
 @SpringBootApplication
 public class StorageSampleApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StorageSampleApplication.class);
+        SpringApplication.run(StorageSampleApplication.class, args);
     }
 }
